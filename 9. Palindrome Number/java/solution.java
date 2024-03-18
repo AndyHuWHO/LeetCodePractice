7c83380d7d13643a2659c39d24b9f3ea0717f3b1
andyhu <andywho@student.ubc.ca>
2024-03-17 17:44:06 -0700

    Palindrome youtube

--- 9. Palindrome Number/java/solution.java
+++ 9. Palindrome Number/java/solution.java
@@ -31,4 +31,22 @@ public class solution {
         return reverse == x;
         
     }
+
+
+    public boolean YoutubeisPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int reverse = 0, copy =x;
+
+       while (copy>0) {
+        int digit = copy % 10;
+        reverse = reverse * 10 + digit;
+        copy /= 10;
+       }
+
+        return reverse == x;
+        
+    }
 }
