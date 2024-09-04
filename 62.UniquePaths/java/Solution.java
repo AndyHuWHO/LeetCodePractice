6c8509408df66c388499b74499b2a8e109fa003d
andyhu <andywho@student.ubc.ca>
2024-09-04 09:00:47 -0700

    non recursive solution

--- 62.UniquePaths/java/Solution.java
+++ 62.UniquePaths/java/Solution.java
@@ -19,5 +19,26 @@ public class Solution {
         memo[m-1][n-1]= findAndMemoPaths(m-1, n, memo) + findAndMemoPaths(m, n-1, memo);
         return memo[m-1][n-1];
     }
+
+    public int uniquePaths (int m, int n) {
+        //initialize the pathGrid that holds num of unique paths
+        int[][] pathGrid = new int[m][n];
+
+        // initialize base case for only 1 column
+        for (int i = 0; i < m; i++) {
+            pathGrid[i][0] = 1;
+        }
+        // initialze base case for only 1 row
+        for (int j = 0; j < n; j++) {
+            pathGrid[0][j] = 1;
+        }
+        // calculate the other grids 
+        for (int i =1; i<m; i++) {
+            for (int j=1; j<n; j++) {
+                pathGrid[i][j] = pathGrid[i][j-1] + pathGrid[i-1][j];
+            }
+        }
+        return pathGrid[m-1][n-1];
+    }
     
 }
